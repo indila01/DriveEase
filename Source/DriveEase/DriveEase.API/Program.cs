d3026d54089627a94a2d6f6c2a67d3d20620cea9
IndilaW <v-rwadugodap@microsoft.com>
2024-01-12 12:11:56 +0800

    healthcheck

--- Source/DriveEase/DriveEase.API/Program.cs
+++ Source/DriveEase/DriveEase.API/Program.cs
@@ -6,6 +6,7 @@ using DriveEase.Persistance.EFCustomizations;
 using DriveEase.SharedKernel;
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using HealthChecks.UI.Client;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
@@ -35,14 +36,19 @@ builder.Services.
     AddFastEndpoints()
     .SwaggerDocument();
 
+
 // options pattern
 builder.Services.Configure<ApplicationConfig>(
     builder.Configuration.GetSection(nameof(ApplicationConfig)));
 
-
-builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+builder.Services
+    .AddHealthChecksUI(options
+    => options
+        .AddHealthCheckEndpoint("HealthCheck API", "/healthcheck"))
+    .AddInMemoryStorage();
 
 builder.Services.AddHealthChecks()
+    .AddDbContextCheck<DriveEaseDbContext>()
     .AddSqlServer(config?.GetConnectionString(Connectionstring.DriveEaseDbConnectionKey));
 
 builder.Services.Add(ServiceDescriptor.Singleton(typeof(IOptionsSnapshot<>), typeof(OptionsManager<>)));
@@ -85,4 +91,11 @@ app.UseCors("DefaultAnyOriginPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapHealthChecks("healthcheck", new()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+});
+
+app.MapHealthChecksUI(options => options.UIPath = "/dashboard");
 app.Run();
