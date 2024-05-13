132c95cc829ab2e419cffe3d5cf8e5bd894311b3
indila dineth <indiladineth@yahoo.com>
2024-05-13 12:14:58 +0800

    auth refactor

--- Source/DriveEase/DriveEase.Application/Actions/Users/Create/CreateUserCommandHandler.cs
+++ Source/DriveEase/DriveEase.Application/Actions/Users/Create/CreateUserCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DriveEase.Domain.Abstraction;
+using DriveEase.Domain.Entities;
 using DriveEase.Domain.Core.Errors;
 using DriveEase.Domain.Repositories;
 using DriveEase.Domain.ValueObjects;
@@ -14,29 +15,37 @@ namespace DriveEase.Application.Actions.Users.Create;
 public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Result<string>>
 {
     /// <summary>
+    /// Gets or sets the Iunit of work.
+    /// </summary>
+    /// <value>
+    /// Iunit of work.
+    /// </value>
+    private IUnitOfWork unitOfWork;
+
+    /// <summary>
     /// Gets or sets the user repository.
     /// </summary>
     /// <value>
     /// The user repository.
     /// </value>
-    private IUserRepository userRepository { get; set; }
-
+    private IUserRepository userRepository;
     /// <summary>
     /// Gets or sets the password hasher.
     /// </summary>
     /// <value>
     /// The password hasher.
     /// </value>
-    private IPasswordHasher passwordHasher { get; set; }
+    private IPasswordHasher passwordHasher;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="CreateUserCommandHandler"/> class.
     /// </summary>
     /// <param name="userRepository">The user repository.</param>
-    public CreateUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher)
+    public CreateUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, IUnitOfWork unitOfWork)
     {
         this.userRepository = userRepository;
         this.passwordHasher = passwordHasher;
+        this.unitOfWork = unitOfWork;
     }
 
     public async Task<Result<string>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
@@ -59,6 +68,13 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Resul
         }
 
         string passwordHash = passwordHasher.HashPassword(password.Value);
+        var user = User.Create(firstName, lastName, email, passwordHash)
+
+        userRepository.Add(user);
+        await this.unitOfWork.SaveChangesAsync(cancellationToken);
+
+        i
+
         return Result.Success(String.Empty);
 
     }
